01c64330dadaf8dbc20f92dc5187a1fa81836728
Kehinde Ojewale <kennyoje1@gmail.com>
2022-04-03 18:33:07 +0100

    First commit

---
+++ assigment1app/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace assigment1app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LAB 1 ASSIGNMENT
+            Console.WriteLine("Hello Kehinde, I am in the backend track of  Wella Schools");
+            Console.WriteLine("I am learning backend development in order to acquire the skills to contribute to the 'backside' aspects in the creation of software solutions to problems across the world ");
+            
+
+            
+
+        }
+    }
+}
